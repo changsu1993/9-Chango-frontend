81b7563e72e88aee84fe0de5e1e3be72cc025dc9
changsu1993 <csdp0106@gmail.com>
2020-07-16 12:27:57 +0900

    footer레이아웃 구현

---
+++ src/Components/Footer/footerList.js
@@ -0,0 +1,45 @@
+const footerList = {
+  list: [
+    {
+      parentsList: '숨고소개',
+      childList: ['회사소개', '채용안내'],
+    },
+    {
+      parentsList: '고객안내',
+      childList: [
+        '이용안내',
+        '안전정책',
+        '예상금액',
+        '고수찾기',
+        '고수에게묻다',
+      ],
+    },
+    {
+      parentsList: '고수안내',
+      childList: ['이용안내', '고수가입', '고수센터', '고수후기'],
+    },
+    {
+      parentsList: '고객센터',
+      childList: ['공지사항', '자주묻는질문'],
+    },
+  ],
+  FooterImage: [
+    {
+      img: 'https://assets.cdn.soomgo.com/icons/icon-footer-sns-facebook.svg',
+    },
+    {
+      img: 'https://assets.cdn.soomgo.com/icons/icon-footer-sns-instagram.svg',
+    },
+    {
+      img: 'https://assets.cdn.soomgo.com/icons/icon-footer-sns-naverblog.svg',
+    },
+    {
+      img: 'https://assets.cdn.soomgo.com/icons/icon-footer-sns-naverpost.svg',
+    },
+    {
+      img: 'https://assets.cdn.soomgo.com/icons/icon-footer-sns-tistory.svg',
+    },
+  ],
+};
+
+export default footerList;
